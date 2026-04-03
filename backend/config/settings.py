e0324a28bed816c7d06c5c02fdfbdd9c64a652b8
vapordiver <dawidkoska217@gmail.com>
2026-04-04 01:50:48 +0200

    feat: configure drf, jwt and token retrieval

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt",
     "corsheaders",
     "django.contrib.gis",
     "storages",
@@ -108,6 +109,15 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+# REST Framework
+# https://www.django-rest-framework.org
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    ]
+}
 
 
 # Internationalization
