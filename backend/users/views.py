06712f1e94e7d8071fc6087ff0f4bac6ca9dc36d
vapordiver <dawidkoska217@gmail.com>
2026-04-05 13:34:17 +0200

    feat: create register endpoint

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer
 
-# Create your views here.
+
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
