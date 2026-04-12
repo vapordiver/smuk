815006a2a4709e789da539b6c80ece313a088431
wisnia <251243@edu.p.lodz.pl>
2026-04-12 18:05:34 +0200

    feat: add serializers for buildings and categories

---
+++ backend/tickets/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Building, FaultCategory
+
+
+class BuildingSerializer(serializers.ModelSerializer):
+    centroid = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Building
+        fields = ["id", "name", "centroid"]
+
+    def get_centroid(self, obj):
+        """
+        Serialize PointField to GeoJSON format
+        """
+        if obj.centroid:
+            return {
+                "type": "Point",
+                "coordinates": [obj.centroid.x, obj.centroid.y],
+            }
+        return None
+
+
+class FaultCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FaultCategory
+        fields = ["id", "name", "icon", "color"]
