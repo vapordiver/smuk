4162e79a247860c68bbe9b6a827f2a175ffca11b
vapordiver <dawidkoska217@gmail.com>
2026-04-15 01:08:19 +0200

    feat: create AuditLogEntry serializer

--- backend/tickets/serializers.py
+++ backend/tickets/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Building, FaultCategory
+from .models import Building, FaultCategory, Ticket, AuditLog
+from users.serializers import UserShortSerializer
 
 
 class BuildingSerializer(serializers.ModelSerializer):
@@ -26,3 +27,15 @@ class FaultCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = FaultCategory
         fields = ["id", "name", "icon", "color"]
+
+
+class AuditLogEntrySerializer(serializers.ModelSerializer):
+    """
+    (`AuditLogEntry` in API contract)
+    """
+    user = UserShortSerializer(read_only=True)
+    class Meta:
+        model = AuditLog
+        fields = ["id", "user", "field_changed",
+         "old_value", "new_value", "created_at"]
+
