5598b32a3a9145d6328b7049b37f51b0b829c6cf
vapordiver <dawidkoska217@gmail.com>
2026-04-15 01:09:22 +0200

    feat: create TicketListItem and TicketDetail serializers

--- backend/tickets/serializers.py
+++ backend/tickets/serializers.py
@@ -39,3 +39,51 @@ class AuditLogEntrySerializer(serializers.ModelSerializer):
         fields = ["id", "user", "field_changed",
          "old_value", "new_value", "created_at"]
 
+
+class TicketListSerializer(serializers.ModelSerializer):
+    """
+    (`TicketListItem` in API contract)
+    """
+    category = FaultCategorySerializer(read_only=True)
+    building = BuildingSerializer(read_only=True)
+    reporter = UserShortSerializer(read_only=True)
+
+    class Meta:
+        model = Ticket
+        fields = ["id", "title", "status", "priority", "category",
+        "building", "reporter", "image", "created_at"]
+
+
+class TicketDetailSerializer(serializers.ModelSerializer):
+    """
+    (`TicketDetail` in API contract)
+    """
+    # mapping audit_log (API contract) -> audit_logs (model relationship)
+    audit_log = AuditLogEntrySerializer(source="audit_logs", many=True, read_only=True)
+    category = FaultCategorySerializer(read_only=True)
+    building = BuildingSerializer(read_only=True)
+    location = serializers.SerializerMethodField()
+    reporter = UserShortSerializer(read_only=True)
+    assigned_to = UserShortSerializer(read_only=True)
+
+    class Meta:
+        model = Ticket
+        fields = [
+            "id", "title", "description", "category", "building",
+            "floor", "room", "status", "priority", "location", "image", 
+            "reporter", "assigned_to", "parent_ticket", "created_at", 
+            "updated_at", 
+            "audit_log" 
+        ]
+
+    def get_location(self, obj):
+        """
+        Serialize location PointField to GeoJSON format
+        """
+
+        if obj.location:
+            return {
+                "type": "Point",
+                "coordinates": [obj.location.x, obj.location.y],
+            }
+        return None
