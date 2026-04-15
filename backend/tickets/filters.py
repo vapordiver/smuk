a7adcb74a7bc2f4525b9552ab7497b0f216126f0
vapordiver <dawidkoska217@gmail.com>
2026-04-15 17:20:25 +0200

    fix: fix filter date logical bug

--- backend/tickets/filters.py
+++ backend/tickets/filters.py
@@ -6,8 +6,8 @@ class TicketFilter(django_filters.FilterSet):
     """
     Special filter class used for `tickets` model.
     """
-    date_from = django_filters.DateFilter(field_name='created_at', lookup_expr='gte')
-    date_to = django_filters.DateFilter(field_name='created_at', lookup_expr='lte')
+    date_from = django_filters.DateFilter(field_name='created_at', lookup_expr='date__gte')
+    date_to = django_filters.DateFilter(field_name='created_at', lookup_expr='date__lte')
 
     category_id = django_filters.NumberFilter(field_name='category_id')
     building_id = django_filters.NumberFilter(field_name='building__id')
