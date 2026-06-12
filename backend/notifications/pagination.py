36ea4802b425acc2e4e5db773992a9a43dabd3d9
vapordiver <dawidkoska217@gmail.com>
2026-06-12 16:38:45 +0200

    feat(notifications): create custom pagination class

---
+++ backend/notifications/pagination.py
@@ -0,0 +1,20 @@
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
+
+from .models import Notification
+
+class NotificationPagination(LimitOffsetPagination):
+    """
+    Pagination class for Notification model in accordance with API contract
+    """
+    def get_paginated_response(self, data):
+        unread_count = Notification.objects.filter(
+            user=self.request.user,
+            is_read=False
+        ).count()
+
+        return Response({
+            "count": self.count,
+            "unread_count": unread_count,
+            "results": data
+        })
