8c9b06f1c8135392c2726dd9857df90198828ca2
Dawid Tchórzewski <251235@edu.p.lodz.pl>
2026-06-15 02:34:12 +0200

    fix: small changes for ticket_generator to fit api contract

--- backend/tickets/management/commands/ticket_generator.py
+++ backend/tickets/management/commands/ticket_generator.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth import get_user_model
 from tickets.models import Building, FaultCategory, Ticket
+from django.utils import timezone
+from datetime import timedelta
 import random
 
 User = get_user_model()
@@ -48,6 +50,13 @@ class Command(BaseCommand):
                 "Nie znaleziono użytkownika reporter1@edu.p.lodz.pl"
             )
 
+        coord = User.objects.filter(
+            email="coord1@p.lodz.pl"
+        ).first()
+
+        if not coord:
+            raise CommandError("Nie znaleziono użytkownika coord1@p.lodz.pl")
+
         buildings = list(Building.objects.all())
         if not buildings:
             raise CommandError("Brak budynków.")
@@ -57,20 +66,18 @@ class Command(BaseCommand):
             raise CommandError("Brak kategorii.")
 
         created = 0
+        now = timezone.now()
 
         for i in range(100):
-
             category = random.choice(categories)
-
             possible_titles = self.TICKETS_BY_CATEGORY.get(
                 category.name,
                 ["Inna usterka"]
             )
-
             title = random.choice(possible_titles)
-
             selected_building = random.choice(buildings)
-
+            days_ago = random.randint(0,180)
+            ticket_date = now - timedelta(days=days_ago)
             ticket = Ticket.objects.create(
                 title=title,
                 description=self.DESCRIPTIONS.get(
@@ -80,7 +87,7 @@ class Command(BaseCommand):
                 building=selected_building,
                 category=category,
                 reporter=reporter,
-                assigned_to=reporter,
+                assigned_to=coord,
                 location=selected_building.centroid,
                 #priority=random.choice([
                 #    Ticket.Priority.LOW,
@@ -94,7 +101,12 @@ class Command(BaseCommand):
                     Ticket.Status.IN_PROGRESS,
                     Ticket.Status.RESOLVED,
                 ]),
+                image = "tickets/2026/06/15/demo.webp" # no such file hehe, but I guess its better to write anything since its required in contract
+                #and if no such file exists frontend will just provide nothing same as if this code wasn't here
             )
+
+            #auto_now_add forces current data so just update it
+            Ticket.objects.filter(pk=ticket.pk).update(created_at=ticket_date, updated_at=ticket_date)
 
             created += 1
 
