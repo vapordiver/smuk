751469cf1f0c2a683991c30faee9251791dd41b9
Filip Piliszek <252819@edu.p.lodz.pl>
2026-06-18 16:33:04 +0200

    feat(config): configure AWS S3 storage with presigned URLs support

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -229,7 +229,8 @@ if USE_S3:
     AWS_S3_SIGNATURE_VERSION = "s3v4"
     AWS_S3_FILE_OVERWRITE = False
 
-    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    # Pre-signed URLs
+    AWS_QUERYING_AUTH = True
+    AWS_QUERYING_EXPIRE = 3600 # link wygasa po godzinie
 
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
+    STORAGES["default"]["BACKEND"] = "storages.backends.s3boto3.S3Boto3Storage"
