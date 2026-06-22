8d8db6eee087cb365119f724885bfcc650ff834a
Filip Piliszek <252819@edu.p.lodz.pl>
2026-06-22 16:08:34 +0200

    fix(infra): formatted code and fixed typo in outputs file

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,15 @@
 output "AWS_BUCKET_NAME" {
-    description = "Bucket name"
-    value = aws_s3_bucket.media_bucket.bucket
+  description = "Bucket name"
+  value       = aws_s3_bucket.media_bucket.bucket
 }
 
 output "AWS_ACCESS_KEY_ID" {
-    description = "Django worker access key ID"
-    value = aws_iam_user.django_user.access_key.id
+  description = "Django worker access key ID"
+  value       = aws_iam_user.django_user_key.id
 }
 
 output "AWS_SECRET_ACCESS_KEY" {
-    description = "Django worker secret access key"
-    value = aws_iam_user.django_user.access_key.secret
-    sensitive = true
+  description = "Django worker secret access key"
+  value       = aws_iam_user.django_user_key.secret
+  sensitive   = true
 }
